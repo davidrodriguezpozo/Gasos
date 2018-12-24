9f1006fde4b0665ff750815b916bd7d5993d3ef3
David Rodriguez <davidrp1996@gmail.com>
2018-12-24 09:24:58 +0100

    Codi fins dia 24/12/18

---
+++ Seccio_constant_compressible.m
@@ -0,0 +1,318 @@
+%----------DINÀMICA DE GASOS-------------
+
+%RESOLUCIÓ ANALÍTICA DE PROBLEMA COMBINAT. 
+%TUB AMB FLUID INTERIOR I CONVECCIÓ EXTERIOR.
+
+
+
+%%-----------CÀLCULS PREVIS---------------
+
+%Discretització del tub, condicions d'entrada...
+clear all;
+Di = 0.01; ri = Di/2; L = 7.659; epsilon = 0.004; R = 287;
+
+gamma = 1.4;
+Min = 0.6;
+Tin = 400;
+vin = sqrt(gamma*R*Tin)*Min; 
+pin = 5e5; 
+Sup = pi*ri^2;
+rhoin = pin/(287*Tin);
+       
+massa = Sup*vin*rhoin;
+
+Tt = 300; %En aquest cas ens donen Ttub;
+
+N = 50; %numero de VC
+
+
+
+delta_x = L/N;
+
+x = 0:delta_x:L;
+
+%Definicio de tots els vectors
+
+S = zeros(N+1,1);
+Cp = zeros(N+1,1);
+A = zeros(N+1,1);
+T_s = zeros(N+1,1);
+V_s = zeros(N+1,1);
+P_s = zeros(N+1,1);
+rho_s = zeros(N+1,1);
+v = zeros(N+1,1);
+T = zeros(N+1,1);
+P = zeros(N+1,1);
+rho = zeros(N+1,1);
+mu_v = zeros(N+1,1);
+lambda = zeros(N+1,1);
+Re_v = zeros(N,1);
+Pr_v = zeros(N,1);
+Nu_v = zeros(N,1);
+alfa_v = zeros(N,1);
+mu_w_v = zeros(N,1);
+Gz_v = zeros(N,1);
+f_v = zeros(N,1);
+q = zeros(N,1);
+T_r = zeros(N,1);
+Sgen = zeros(N,1);
+Sgen1_v = zeros(N,1);
+Sgen2_v = zeros(N,1);
+det_v = zeros(N,1);
+P1_v = zeros(N,1);
+P2_v = zeros(N,1);
+T1_v = zeros(N,1);
+T2_v = zeros(N,1);
+v1_v = zeros(N,1);
+v2_v = zeros(N,1);
+
+Trs = Tin; %Suposem la temperatura de recuperació inicial
+
+%Discretitzem a tots els nodes (variables que no canvien), i omplim els
+%vectors amb les variables suposades.
+for i = 1:N+1
+    S(i) = pi*ri^2;
+    A(i) = Di*pi*delta_x;
+    T_s(i) = Tin;
+    V_s(i) = vin;
+    P_s(i) = pin;
+    rho_s(i) = rhoin;
+end
+
+for i = 1:N+1
+    S(i) = pi*ri^2;
+    A(i) = Di*pi*delta_x;
+    T(i) = Tin;
+    v(i) = vin;
+    P(i) = pin;
+    rho(i) = rhoin;
+end
+y = 1;
+
+%% Resolució del problema
+    
+for i = 1:N
+    i 
+    T_s(i+1) = T(i);
+    P_s(i+1) = P(i);
+    V_s(i+1) = v(i);
+    rho_s(i+1) = rho(i);
+    %dif = 10e10; %Valor arbitrari per poder iterar un altre cop
+    
+    %while dif > 1
+    y = 1;
+    dif2 = 30;  
+        while dif2 > 1e-10 %Iteracions per trobar el valor de Tr
+ 
+    Ti = 0.5*(Tt+Tin)+0.22*(Trs-Tin); %Tr anira canviant a cada iteració;
+    vi = 0.5*(v(i)+v(i+1));
+    Pi = 0.5*(P(i)+P(i+1));
+    rhoi = Pi/(287*Ti);
+    
+    if Ti < 1500 %Càlcul de mu
+       %mu = (1.458e-6*Ti^1.5)/(Ti+110.4);
+       mu = (2.5393e-5*sqrt(Ti/273.15))/(1+122/Ti);
+    else
+       mu = (2.5393e-5*sqrt(Ti/273.15))/(1+122/Ti);
+    end
+   
+    if Tt < 1500  %càlcul de mu_w
+       %mu_w = (1.458e-6*Tt^1.5)/(Tt+110.4);
+       mu_w = (2.5393e-5*sqrt(Tt/273.15))/(1+122/Tt);
+    else
+       mu_w = (2.5393e-5*sqrt(Tt/273.15))/(1+122/Tt);
+    end
+   
+    %lambda = (2.648e-3*sqrt(Ti))/(1+(245.4/Ti)*10^(-12/Ti));
+    lambda = 3.807e-3+7.4e-5*Ti;
+    %Cpi = 1034.09-2.849e-1*Ti+7.817e-4*Ti^2-4.971e-7*Ti^3+1.077e-10*Ti^4;
+    Cpi = 1022 - 0.166*Ti + 3.5025e-4*Ti^2;
+    
+    Re = rhoi*vi*Di/mu;
+    
+    Pr = mu*Cpi/lambda;
+    
+    Gz = (pi*Di)/(4*L)*Re*Pr;
+    
+    if Re < 2000
+       r = Pr^(1/2);
+    else
+       r = Pr^(1/3);
+    end
+    
+    Tr = Tin + r*vi^2/(2*Cpi);
+    
+    dif2 = abs(Tr-Trs);
+    Trs = Tr;
+    y = y+1;
+    dif2(y) = dif2;
+        end
+    
+    %Aqui ja hem calculat el valor real de Tr, i per tant de Ti.
+    %Guardem les dades en vectors per comprobar resultats
+    
+    Cp(i) = Cpi;
+    Re_v(i) = Re;
+    Pr_v(i) = Pr;
+    Gz_v(i) = Gz;
+    mu_v(i) = mu;
+    mu_w_v(i) = mu_w;
+    
+    
+    % Càlcul del coef. de fricció
+    
+    if Re < 2000
+        f = 16 * Re^-1;
+    elseif (5e3 < Re) && (Re < 3e4) 
+        f = 0.079 * Re^-0.25;
+    else
+        f = 0.046 * Re^-0.2;
+    end
+    
+    if epsilon == 0.004
+        if Re < 2000
+        f = 16 * Re^-1;
+        elseif (5e3 < Re) && (Re < 3e4) 
+        f = 0.096 * Re^-0.25;
+        else
+        f = 0.078 * Re^-0.2;
+        end
+    end
+    
+    
+    %Càlcul del Nussel i de alfa
+    
+    if Re < 2000 && Gz> 10 %estem a flux laminar, tub curt. 
+       C = 1.86; m = 1/3; n = 1/3; K = (Di/L)^(1/3)*(mu/mu_w)^0.14;
+    
+    elseif Re < 2000 && Gz < 10 %Flux Laminar, tub llarg.
+       C = 3.66; m = 0; n = 0; K = 1;
+    end
+    
+    if  Re > 2000
+        C = 0.023; m=0.8; n=0.4; K = 1;
+    end
+    
+    Nu = C*Re^m*Pr^n*K;
+    alfa = lambda*Nu/Di;
+    Nu_v(i) = Nu;
+    f_v(i) = f;
+    alfa_v(i) = alfa;
+    
+    q(i) = alfa*(Tt-Tr)*pi*Di*delta_x; %podem calcular el flux de calor amb Tr;
+    
+    %Resolem el sistema d'equacions
+     
+    syms pres;
+    syms Temp;
+    syms vel;
+    syms dens;
+    
+
+    A_v = massa + f*rhoi*vi/4*pi*Di*delta_x;
+    B_v = Sup;
+    C_v = Sup*P(i)+(massa-f*rhoi*vi/4*pi*Di*delta_x)*v(i);
+    
+    A_t = massa*Cpi + 0.5*(alfa*Di*pi*delta_x);
+    B_t = 0.5*massa + (r*alfa*Di*pi*delta_x)/(4*Cpi);
+    C_t = (massa*Cpi - alfa*Di*delta_x*pi*0.5)*T(i)+(0.5*massa-(r*alfa*Di*pi*delta_x)/(4*Cpi))*v(i)^2+ alfa*Tt*Di*pi*delta_x;
+    
+    A = A_v*A_t*Sup - B_v*B_t*massa*R;
+    B = C_v*A_t*Sup;
+    C = B_v*C_t*massa*R;
+    
+    syms vel;
+    eqn1 = A*vel^2 - B*vel + C == 0;
+    
+    det = B^2-4*A*C;
+    det_v(i) = det;
+    if B^2-4*A*C < 0 
+        break;
+    else
+    v_sol = solve(eqn1, vel);
+    end
+    v1 = double(v_sol(1));
+    v2 = double(v_sol(2));
+    v1_v(i) = v1;
+    v2_v(i) = v2;
+    
+    Vol = Sup*delta_x;
+    P1 = (C_v-A_v*v1)/B_v;
+    T1 = (C_t-B_t*v1^2)/A_t;
+    P2 = (C_v-A_v*v2)/B_v;
+    T2 = (C_t-B_t*v2^2)/A_t;
+    
+    P1_v(i) = P1;
+    P2_v(i) = P2;
+    T2_v(i) = T2;
+    T2_v(i) = T1;
+    
+    Sgen1 = 1/Vol*(massa*(Cpi*log(T1/T(i))-R*log(P1/P(i))) - q(i)*Di*pi*delta_x/Tt);
+    Sgen2 = 1/Vol*(massa*(Cpi*log(T2/T(i))-R*log(P2/P(i))) - q(i)*Di*pi*delta_x/Tt);
+    
+    Sgen1_v(i) = Sgen1;
+    Sgen2_v(i) = Sgen2;
+    
+    if Sgen1 < 0 && Sgen2 < 0
+        break;
+    end
+    
+    if abs(Sgen1) > abs(Sgen2) %Ens quedem amb la que genera menys entropia
+        v(i+1) = v2;
+        T(i+1) = T2;
+        P(i+1) = P2;
+        Sgen(i) = Sgen2;
+    else
+        v(i+1) = v1;
+        T(i+1) = T1;
+        P(i+1) = P1;
+        Sgen(i) = Sgen1;
+    end
+    
+    rho(i+1) = P(i+1)/(R*T(i+1));
+    
+    %Guardem i comprovem les diferències entre suposat-calculat.
+    
+    difvector(1) = abs(v(i+1)-V_s(i+1));
+    difvector(2) = abs(T(i+1)-T_s(i+1));
+    difvector(3) = abs(P(i+1)-P_s(i+1));
+    
+    dif = max(difvector);
+
+    %Canviem el vector suposat pel calculat i tornem a iterar.
+    
+    V_s(i+1) = v(i+1);
+    P_s(i+1) = P(i+1);
+    T_s(i+1) = T(i+1);      
+    x(i)
+   %end
+    
+end
+Q = sum(q)
+T_out = T(N+1)
+P_out = P(N+1)
+v_out = v(N+1)
+Re_out = Re_v(N)
+Pr_out = Pr_v(N)
+
+
+
+
+%% Plots per les diferents variables
+figure;
+plot(x,T);
+title('Temperatura en funció de x');
+
+figure;
+plot(x,P);
+title('Pressió en funció de x');
+
+figure;
+plot(x,v);
+title('Velocitat en funció de x');
+
+figure;
+plot(x,rho);
+title('Densitat en funció de x');
+
+
---
+++ Seccio_constant_incompressible.m
@@ -0,0 +1,281 @@
+%----------DINÀMICA DE GASOS-------------
+
+%RESOLUCIÓ ANALÍTICA DE PROBLEMA COMBINAT. 
+%TUB AMB FLUID INTERIOR I CONVECCIÓ EXTERIOR.
+
+
+
+%%-----------CÀLCULS PREVIS---------------
+
+%Discretització del tub, condicions d'entrada...
+clear all;
+Di = 0.02; ri = Di/2; L = 5; epsilon = 0.0001;
+
+water = false; air = true; oil = false; %Tipus de liquid
+
+vin = 30; 
+pin = 2e5; 
+Tin = 20+273.15; 
+Sup = pi*ri^2;
+
+
+if water
+rhoin = 847.2 + 1.298*Tin - 2.657e-3*Tin^2;
+elseif oil
+rhoin = 1164.45 - 0.4389*Tin-3.21e-4*Tin^2;
+else
+rhoin = pin/(287*Tin);
+end
+       
+massa = Sup*vin*rhoin;
+
+Tt = 95+273.15; %En aquest cas ens donen Ttub;
+
+N = 50; %numero de VC
+
+
+
+delta_x = L/N;
+
+x = 0:delta_x:L;
+
+%Definicio de tots els vectors
+
+S = zeros(N+1,1);
+Cp = zeros(N+1,1);
+A = zeros(N+1,1);
+T_s = zeros(N+1,1);
+V_s = zeros(N+1,1);
+P_s = zeros(N+1,1);
+rho_s = zeros(N+1,1);
+v = zeros(N+1,1);
+T = zeros(N+1,1);
+P = zeros(N+1,1);
+rho = zeros(N+1,1);
+mu_v = zeros(N+1,1);
+lambda = zeros(N+1,1);
+Re_v = zeros(N,1);
+Pr_v = zeros(N,1);
+Nu_v = zeros(N,1);
+alfa_v = zeros(N,1);
+mu_w_v = zeros(N,1);
+Gz_v = zeros(N,1);
+f_v = zeros(N,1);
+q = zeros(N,1);
+%Discretitzem a tots els nodes (variables que no canvien), i omplim els
+%vectors amb les variables suposades.
+for i = 1:N+1
+    S(i) = pi*ri^2;
+    A(i) = Di*pi*delta_x;
+    T_s(i) = Tin;
+    V_s(i) = vin;
+    P_s(i) = pin;
+    rho_s(i) = rhoin;
+end
+
+for i = 1:N+1
+    S(i) = pi*ri^2;
+    A(i) = Di*pi*delta_x;
+    T(i) = Tin;
+    v(i) = vin;
+    P(i) = pin;
+    rho(i) = rhoin;
+end
+
+
+%% Resolució del problema
+    
+for i = 1:N
+    i 
+    T_s(i+1) = T(i);
+    P_s(i+1) = P(i);
+    V_s(i+1) = v(i);
+    rho_s(i+1) = rho(i);
+    dif = 10e10; %Valor arbitrari per poder iterar un altre cop
+    
+    while dif > 10
+        
+    Ti = 0.5*(T(i)+T(i+1));
+    Pi = 0.5*(P(i)+P(i+1));
+    
+    if water
+    rhoi = 847.2 + 1.298*Ti - 2.657e-3*Ti^2;
+    elseif oil
+    rhoi = 1164.45 - 0.4389*Ti-3.21e-4*Ti^2;
+    else
+    rhoi = Pi/(287*Ti);
+    end
+    
+    vi = 0.5*(v(i)+v(i+1));
+    
+    
+    if water %Tractem el cas de l'aigua
+    
+       
+    if Ti < 353 %Càlcul de mu
+       mu = 0.9149 -1.2563e-2*Ti+6.9182e-5*Ti^2-1.9067e-7*Ti^3+2.6275e-10*Ti^4-1.4474e-13*Ti^5;
+    else
+       mu = 3.7471e-2-3.5636e-4*Ti + 1.3725e-6*Ti^2-2.6566e-9*Ti^3+2.5766e-12*Ti^4-1e-15*Ti^5;
+    end
+    
+    if Tt < 353 %càlcul de mu_w
+        mu_w = 0.9149 - 1.2563e-2*Tt+ 6.9182e-5*Tt^2 - 1.9067e-7*Tt^3 + 2.476e-10*Tt^41.4474e-13*Tt^5;
+    else
+        mu_w = 3.7471e-2 - 3.5636e-4*Tt + 1.3725e-6*Tt^2 - 2.6566e-9*Tt^3 + 2.5766e-12*Tt^4-1e-15*Tt^5;
+    end
+    
+    lambda = -1.176 + 7.915e-3*Ti + 1.486e-5*Ti^2 - 1.317e-7*Ti^3 + 2.476e-10*Ti^4 - 1.556e-13*Ti^5;
+    
+    Cpi = 5648.8 - 9.140*Ti + 14.21e-3*Ti^2;
+    
+    elseif air %Ara tractem el cas de l'aire
+    
+    if Ti < 1500 %Càlcul de mu
+       mu = (1.458e-6*Ti^1.5)/(Ti+110.4);
+    else
+       mu = (2.5393e-5*sqrt(Ti/273.15))/(1+122/Ti);
+    end
+   
+    if Tt < 1500  %càlcul de mu_w
+       mu_w = (1.458e-6*Tt^1.5)/(Tt+110.4);
+    else
+       mu_w = (2.5393e-5*sqrt(Tt/273.15))/(1+122/Tt);
+    end
+    
+    lambda = (2.648e-3*sqrt(Ti))/(1+(245.4/Ti)*10^(-12/Ti));
+    
+    Cpi = 1034.09-2.849e-1*Ti+7.817e-4*Ti^2-4.971e-7*Ti^3+1.077e-10*Ti^4;
+    
+    else %Tractem el cas de l'oli
+    
+    mu = rhoi*exp(-16.096+(586.38/(Ti-210.65))); %càlcul de mu
+   
+    mu_w = rhoi*exp(-16.096+(586.38/(Tt-210.65))); %calcul de mu_w
+    
+    lambda = 0.116 + 4.9e-5*Ti - 1.5e-7*Ti^2;
+    
+    Cpi = 658 + 2.82*Ti + 8.97e-4*Ti^2;    
+        
+    end
+    Re = rhoi*vi*Di/mu;
+    
+    Pr = mu*Cpi/lambda;
+    
+    Gz = (pi*Di)/(4*L)*Re*Pr;
+    
+    %Guardem les dades en vectors per comprobar resultats
+    
+    Cp(i) = Cpi;
+    Re_v(i) = Re;
+    Pr_v(i) = Pr;
+    Gz_v(i) = Gz;
+    mu_v(i) = mu;
+    mu_w_v(i) = mu_w;
+    
+    
+    % Càlcul del coef. de fricció
+    
+    if Re < 2000
+        f = 16 * Re^-1;
+    elseif (5e3 < Re) && (Re < 3e4) 
+        f = 0.079 * Re^-0.25;
+    else
+        f = 0.046 * Re^-0.2;
+    end
+    
+    
+    %Càlcul del Nussel i de alfa
+    
+    if Re < 2000 && Gz> 10 %estem a flux laminar, tub curt. 
+       C = 1.86; m = 1/3; n = 1/3; K = (Di/L)^(1/3)*(mu/mu_w)^0.14;
+    
+    elseif Re < 2000 && Gz < 10 %Flux Laminar, tub llarg.
+       C = 3.66; m = 0; n = 0; K = 1;
+    else %Flux turbulent.
+        C = 0.027; m= 0.8; n = 0.33; K = (mu/mu_w)^0.14;
+    end
+    
+    if air && Re > 2000
+        C = 0.023; m=0.8; n=0.4; K = 1;
+    end
+    
+    Nu = C*Re^m*Pr^n*K;
+    alfa = lambda*Nu/Di;
+    Nu_v(i) = Nu;
+    f_v(i) = f;
+    alfa_v(i) = alfa;
+    
+    %Resolem el sistema d'equacions
+     
+    syms pres;
+    syms Temp;
+    syms vel;
+
+    eqn1 = massa*(v(i+1)-v(i)) == P(i)*Sup-pres*Sup-0.5*f*rhoi*vi^2*pi*Di*delta_x;
+    eqn2 = massa*Cpi*(Temp-T(i)) + massa*0.5*(v(i+1)^2-v(i)^2) == alfa*(Tt-0.5*(T(i)+Temp))*pi*Di*delta_x;
+    
+    P(i+1) = solve(eqn1, pres);
+    T(i+1) = solve(eqn2, Temp);
+    
+    %càlcul de les diferents densitats per separat.
+    
+    if water
+    rho(i+1) = 847.2 + 1.298*T(i+1) - 2.657e-3*T(i+1)^2;
+    elseif oil
+    rho(i+1) = 1164.45 - 0.4389*T(i+1)-3.21e-4*T(i+1)^2;
+    else
+    rho(i+1) = P(i+1)/(287*T(i+1));    
+    end
+    eqn3 = massa == rho(i+1)*vel*Sup;
+    
+    v(i+1) = solve(eqn3, vel);
+    
+    q(i) = alfa*(Tt-0.5*(T(i)+T(i+1)))*pi*Di*delta_x;
+   
+    
+
+    %Guardem i comprovem les diferències entre suposat-calculat.
+    
+    difvector(1) = abs(v(i+1)-V_s(i+1));
+    difvector(2) = abs(T(i+1)-T_s(i+1));
+    difvector(3) = abs(P(i+1)-P_s(i+1));
+    
+    dif = max(difvector);
+
+    %Canviem el vector suposat pel calculat i tornem a iterar.
+    
+    V_s(i+1) = v(i+1);
+    P_s(i+1) = P(i+1);
+    T_s(i+1) = T(i+1);      
+    
+   end
+    
+end
+Q = sum(q);
+T_out = T(N+1)-273.15;
+P_out = P(N+1);
+v_out = v(N+1);
+Re_out = Re_v(N);
+Pr_out = Pr_v(N);
+
+
+
+
+%% Plots per les diferents variables
+figure;
+plot(x,T);
+title('Temperatura en funció de x');
+
+figure;
+plot(x,P);
+title('Pressió en funció de x');
+
+figure;
+plot(x,v);
+title('Velocitat en funció de x');
+
+figure;
+plot(x,rho);
+title('Densitat en funció de x');
+
+
